0abd187968d6a335d9432278f2e44c7e8aee0cfa
MeravatPradeep <Pradeep.Meravat@viriminfotech.com>
2024-01-18 06:11:31 -0500

    added linter changes

--- models/base/dbt_mapped_tracks.sql
+++ models/base/dbt_mapped_tracks.sql
@@ -6,15 +6,17 @@
 
 {{ config(materialized='table') }}
 
-select *
-        ,timestampdiff(minutes, cast(timestamp as timestamp), cast(lag(timestamp) over(partition by dbt_visitor_id order by timestamp) as timestamp)) as idle_time_minutes
-      from (
-        select t.id as event_id
-          ,t.anonymous_id
-          ,a2v.dbt_visitor_id
-          ,t.timestamp
-          ,t.event as event
-        from {{ source("<schema>","tracks")}} as t
-        inner join {{ ref('dbt_aliases_mapping') }} as a2v
+select
+    *,
+    timestampdiff(minutes, cast(timestamp as timestamp), cast(lag(timestamp) over (partition by dbt_visitor_id order by timestamp) as timestamp)) as idle_time_minutes
+from (
+    select
+        t.id as event_id,
+        t.anonymous_id,
+        a2v.dbt_visitor_id,
+        t.timestamp,
+        t.event as event
+    from {{ source("schema","tracks")}} as t
+    inner join {{ ref('dbt_aliases_mapping') }} as a2v
         on a2v.alias = coalesce(t.user_id, t.anonymous_id)
-        )
+)
--- models/base/dbt_tracks_flow.sql
+++ models/base/dbt_tracks_flow.sql
@@ -1,34 +1,34 @@
 /*
 
-We leverage analytic functions like first_value and nth_value to create 5-event sequences that capture the flow of events during a session. 5 can be increased or decreased as per requirements.
+we leverage analytic functions like first_value and nth_value to create 5-event sequences that capture the flow of events during a session. 5 can be increased or decreased as per requirements.
 
 */
 
 {{ config(materialized='table') }}
 
 with derived_table as (
-          select
-            event_id,
-            session_id,
-            track_sequence_number,
-            first_value(event IGNORE NULLS) over(partition by session_id order by track_sequence_number asc) as event,
-            dbt_visitor_id,
-            timestamp,
-            nth_value(event,2) IGNORE NULLS over(partition by session_id order by track_sequence_number asc) as second_event,
-            nth_value(event,3) IGNORE NULLS over(partition by session_id order by track_sequence_number asc) as third_event,
-            nth_value(event,4) IGNORE NULLS over(partition by session_id order by track_sequence_number asc) as fourth_event,
-            nth_value(event,5) IGNORE NULLS over(partition by session_id order by track_sequence_number asc) as fifth_event
-            from {{ ref('dbt_track_facts') }}
-        )
-
-          select event_id
-            , session_id
-            , track_sequence_number
-            , event
-            , dbt_visitor_id
-            , cast(timestamp as timestamp) as timestamp
-            , second_event as event_2
-            , third_event as event_3
-            , fourth_event as event_4
-            , fifth_event as event_5
-      from derived_table a
+    select
+        event_id,
+        session_id,
+        track_sequence_number,
+        first_value(event ignore nulls) over (partition by session_id order by track_sequence_number asc) as event,
+        dbt_visitor_id,
+        timestamp,
+        nth_value(event, 2) ignore nulls over (partition by session_id order by track_sequence_number asc) as second_event,
+        nth_value(event, 3) ignore nulls over (partition by session_id order by track_sequence_number asc) as third_event,
+        nth_value(event, 4) ignore nulls over (partition by session_id order by track_sequence_number asc) as fourth_event,
+        nth_value(event, 5) ignore nulls over (partition by session_id order by track_sequence_number asc) as fifth_event
+    from {{ ref('dbt_track_facts') }}
+)
+select
+    event_id,
+    session_id,
+    track_sequence_number,
+    event,
+    dbt_visitor_id,
+    cast(timestamp as timestamp) as timestamp,
+    second_event as event_2,
+    third_event as event_3,
+    fourth_event as event_4,
+    fifth_event as event_5
+from derived_table a
